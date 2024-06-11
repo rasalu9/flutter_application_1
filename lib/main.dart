56468c80cd9063b204de365877cf3272574f5068
rasalu9 <136328287+rasalu9@users.noreply.github.com>
2024-06-11 15:39:01 +0530

    container

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
 
   // This widget is the root of your application.
   @override
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AssignmentOne(),
+      home: AssignmentOne(),
     );
   }
 }
